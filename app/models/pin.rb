278c28c700df2610a0966ca16eecdb8fb5314f13
Sharnie Ivery <sharnieivery@gmail.com>
2014-02-02 14:02:44 -0500

    Add image upload with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/assets/avatar-blank.jpg'
 end
