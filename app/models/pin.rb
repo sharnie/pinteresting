ce4ff2044a7715093ad03a2be398a3eec23c9221
Sharnie Ivery <sharnieivery@gmail.com>
2014-02-03 00:07:22 -0500

    Update show view, add glyphicons, and change root path

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/assets/avatar-blank.jpg'
+	has_attached_file :image, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/assets/avatar-blank.jpg'
 end
